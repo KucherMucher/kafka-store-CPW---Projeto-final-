b46d9f6c93bea803e1f3b3cd0e177ee650120d37
Kucher Mucher <a10653@csmiguel.pt>
2025-12-19 11:02:33 +0000

    19/12/25

--- js/carousel.js
+++ js/carousel.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     initCarousel();
 });
 
+const carouselContainer = document.querySelector('#carousel-container');
+
 function initCarousel(){
-    const carouselContainer = document.querySelector("#carousel-container");
+    
 
     if(!carouselContainer){
         console.log("carousel Conainer not found");
@@ -19,48 +21,89 @@ function initCarousel(){
 const imagesCarousel = [ //struct list
     {
         id: 1,
-        src: '../images/carousel1.jpg',
+        src: 'images/carousel1.png',
         pageLink: '../page.html'
     },
     {
         id: 2,
-        src: '../images/carousel2.jpg',
+        src: 'images/carousel2.png',
         pageLink: '../page.html'
 
         
     },
     {
         id: 3,
-        src: '../images/carousel3.jpg',
+        src: 'images/carousel3.png',
         pageLink: '../page.html'
         
     },
     {
         id: 4,
-        src: '../images/carousel4.jpg',
+        src: 'images/carousel4.png',
         pageLink: '../page.html'
         
     },
     {
         id: 5,
-        src: '../images/carousel5.jpg',
+        src: 'images/carousel5.png',
         pageLink: '../page.html'
         
     },
     {
         id: 6,
-        src: '../images/carousel6.jpg',
+        src: 'images/carousel6.png',
         pageLink: '../page.html'
         
-    },
-    {
-        id: 7,
-        src: '../images/carousel7.jpg',
-        pageLink: '../page.html'
-        
-    },
+    }
+    
 ];
 
+function createCarouselElemenets(){
+    carouselContainer.innerHTML = '';
 
+    imagesCarousel.forEach((image, id) => {
+        const carousel_element = document.createElement('img');
+        carousel_element.src = image.src;
+        carousel_element.className = 'carousel-element';
+        carousel_element.dataset.id = id;
+
+        carouselContainer.appendChild(carousel_element);
+    });
+
+    console.log("Created carousel images")
+}
+
+function createCarouselImage(){
+    
+}
+
+function createCarouselButton(){
+
+}
+
+function createCarouselMoveButtons(){
+
+}
+
+
+
+function initCarousel(){
+    console.log("Init carousel...")
+
+    try{
+        if(!carouselContainer){
+            throw new Error('Didnt find necessary elements')
+        }
+
+        createCarouselElemnets();
+
+        console.log('created carousel elements')
+    }
+    catch (error){
+        console.error('Error init carousel', error)
+        alert('There was an error setting carousel elemetns. Reload the page.')
+    }
+
+}
 
 
