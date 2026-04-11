dce097229f2ade166a712ee3d36baeaa01898bc1
snak369 <snak3srul3s@gmail.com>
2026-04-11 14:31:15 +0100

    pull for vm

--- novo-projeto-final/backend/connect.php
+++ novo-projeto-final/backend/connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = ""; // aluno if using vm
-$db   = "om_db_test";
+$pass = "aluno"; // aluno if using vm
+$db   = "om_db"; // _test if using laragon
 
 $mysqli = new mysqli($host, $user, $pass, $db);
 
